927e00f0da441142c3b22c28501dbe1379281ac2
unknown <SENA@MABAPRCNCFSW007.FormacionSena.RED>
2023-03-09 16:21:22 -0500

    MD -> html, javascript, py

---
+++ javascript/IngreseName.js
@@ -0,0 +1,57 @@
+var usuariocreado;
+var num = 1;
+
+for ( i = 0; i < num; i++) {
+
+    var Login_Or_Regist = prompt("Bienvenido a JS!\nTiene cuenta en esta app?");
+
+    if (Login_Or_Regist == "si") {
+
+        var validar_nombre = prompt("Ingrese su nombre de usuario para validar");
+        var validar_passwd = prompt("Ingrese su contraseña");
+
+        if (contraseña_creado == validar_passwd || usuariocreado == validar_nombre) {
+            alert("Bienvenido "+usuariocreado+"!");
+        } else{
+            alert("ERROR\nFavor ingrese bien la contraseña u usuario");
+            num++;
+        }
+
+    } else if (Login_Or_Regist == "no") {
+        var h = 1;
+        for (let j = 0; j < h; j++) {
+
+            var nombre_usuario_reg = prompt("Ingrese un nombre de usuario para el registro");
+            var passwd = prompt("Ingrese una contraseña");
+            var confirmacion = prompt("Quiere tener '" + nombre_usuario_reg + "' como nombre de usuario?");
+
+            if (confirmacion == "si") {
+
+                var conf_usuario = prompt("Ingrese nuevamente su nombre de usuario ");
+                var conf_passwd = prompt("Ingrese su contraseña de usuario");
+
+                if (conf_usuario == nombre_usuario_reg || conf_passwd == passwd) {
+                                        
+                    usuariocreado = conf_usuario;
+                    contraseña_creado = conf_passwd;
+                    alert("Se ha registrado con exito! "+ usuariocreado);
+                    break;                    
+
+                } else {
+                    alert("Favor ingrese bien la contraseña o usuario")
+                    h++;
+                }
+
+            } else if (confirmacion == "no") {
+                h++;
+            } else {
+                alert("Favor ingrese (si/no)");
+                h++;
+            }
+
+        }
+    } else{
+        alert("Solamente ingrese (si/no)");
+        num++;
+    }
+}
---
+++ python/app.py
@@ -0,0 +1,183 @@
+#Inicializacion de variables
+'''
+x = float(4)
+name = ""
+
+''Seccion de comentarios --vowel(3)''
+
+conff = input("Menu?\n")
+
+if conff == "y":
+    #Menu - Selector
+    Nav = int(input("1.Nombres\n2.Match\n3"))
+    def menu(no):
+        switch={
+        1:logNames(),
+        2:Match(),
+        3:'i',
+        4:'o',
+        5:'u'
+        }
+        return switch.get(no,"input")
+       
+    def logNames():        
+        #.1) Mostrar nombres
+        name = input("# Mostrar nombres\nComo te llamas?\n")
+        return print("Tu nombre es "+name+"?")
+
+    def Match():        
+        #.2) Sumar 2 numeros y mostar un resultado --Match
+        number = input("Dame un numero:\n")
+        one = int(number)
+        number = input("Dame otro numero:\n")
+        two = int(number)
+        number = one + two
+        return print(f'Number: {number}')
+else:
+    num = int(input("Favor digite un numero\n"))
+    if num > 0:
+        print(f"El valor del numero es: {num} Positivo")
+    else:
+        print(f"El valor del numero es: {num}")'''
+
+
+# 2.00 = Lunes, Martes, miercoles; 2.50 = jueves y viernes; 3.00 = sabados y domingos
+'''Time limit: 5mnt || diaSemana != dias : error'''
+
+
+'''
+diaSemanas = input("Seleccione dias de la semana: 'lunes','martes','miercoles','jueves','viernes','sabados','domingos':\n")
+time = int(input("Seleccione el tiempo en minutos:\n"))
+#costo = float(0)
+def cobroHour(costo):
+    costo = float(time * costo)
+    print(f"Costo estipúlado es:\n${costo}")
+     
+
+if diaSemanas == 'lunes' or diaSemanas == 'martes' or diaSemanas == 'miercoles':
+    # var = 2.00 || var = time * var
+    if time > 5:
+        costo = 2.00        
+        cobroHour(costo)
+    else:
+        print("Costo estipúlado:\n$0.00")
+    
+
+if diaSemanas == 'jueves' or diaSemanas == 'viernes':
+    #2.50
+    if time > 5:
+        costo = 2.50        
+        cobroHour(costo)
+    else:
+            print("Costo estipúlado:\n$0.00")  
+if diaSemanas == 'sabados' or diaSemanas == 'domingos':
+    #3.00
+    if time > 5:
+        costo = 3.00        
+        cobroHour(costo)
+    else:
+        print("Costo estipúlado:\n$0.00")
+'''
+#XD = """Esto se supone que es un comentario, pero vamos a probar si sirve XD, bueno. Espero que funcione,...\nDespues de todo si fallo no pasa nada y puedo preguntarle al profe hasta que se estrece(Excelente plan).\nEnd Fin"""
+
+#Ciclos Repetitivos
+'''def ciclosRepetitivos():
+        
+    for x in range(2,12,2):
+        print(x)
+    print(XD)'''
+
+'''for x in "banana":
+    print(x)'''
+
+'''for x in range(6):
+    if x == 6:break
+    print(x)
+else:
+        print("Finished")'''
+
+'''i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("var 'i' no es menor a 6")'''
+
+#Strings are Arrays
+'''txt = "The best things in life are free!"
+print(len(txt))
+print("free" in txt)'''
+
+'''txt = "The best things in life are free!"
+if "free" in txt:
+    print("Encontrado!\n")
+else:
+    print("Sorry!, no se encontro la palabra\n")# "abcdefghijklmnopqrstuvwxyz"
+abc = -1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,0,1,2,3,4,5,6,7,8,9,10,11
+print(abc[-12:])
+
+a = "Hello world!"
+print(a.upper())
+print(a.strip())#Return "Hello world!"
+print(a.lower())
+print(a.replace("H","J"))
+print(a.split(","))#Return ["Hello0 world"]
+'''
+#String Format
+'''age = 36
+txt = "My name is William, I am "+ str(age) # Conversion a Str
+print(txt)
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to play {} dollars for {} pieces of item {}." # Inyectar variables | Posiciones: {2} - {0} - {1}
+print(myorder.format(quantity, itemno, price))'''
+
+pyDoc = '''#Cap 2 || MANEJO DE DATOS <>\n
+Admnistracion de Lista.
+
+Estas pueden usarse para el manejo de datos. Empecemos por su uso:
+
+|> Simple
+
+|-> thislist = ["apple","banana","cherry"]\nprint(thislist) //otros usos : len.(longitud) >> 5 | type.(tipo-dato) >> class 'list'
+
+|> Posiciones
+
+|-> thislist = ['apple','banana','cherry','blackberry','mango']
+
+|-> print(thislist[1]) /#/ Hubicacion de una posicion de la lista = {0:inicio, 1:Consecutivo, etc...}
+
+|-> print(thislist[-1]) /#/ Hubicacion en la ultima posicion de la lista
+
+|-> print(thislist[1:4]) /#/ Hubicacion - dentro de un rango de la lista
+/>Ej: {1, 2, 3, 4, 5} | Rango(1:4) >> print( 2 - 3 - 4 );
+
+|-> print(thislist[-4:-1]) /#/ Hubicacion - dentro de un rango de la lista (Iniciando desde atras)
+/>Ej: {1, 2, 3, 4, 5} | Rango(-4:-1) >> print( 2 - 3 - 4 - 5);
+
+|-> print(thislist[:4]) /#/ Hubicacion - dentro de un rango de la lista (el "vacio" significa la primera posicion independientemente si esta en el inicio o al final)
+/>Ej: [:4] => 1-4 || [1:] => 2-4
+
+|> Filtro
+
+|-> thislist = ['apple','banana','cherry','blackberry','mango'] /#/ Para poder hacer un filtro de una lista de una determinada palabra se puede usar el siguiente code.
+/> if "apple" in thislist:
+        print("yes, 'apple' is in this list")
+
+|> Insert
+
+|-> thislist = ['apple','banana','cherry','blackberry','mango'] /#/ Se puede insertar uno o varios datos dentro de una Lista -> En este caso por posiciones 
+/> thislist[1:3] = ["blackcurrant","watermelon"] \n print(thislist)
+
+>> Tambien se puede de esta forma: 'thislist.insert(2, "watermelon")'
+'''
+#Insertar la lista
+'''thislist = ['apple','banana','cherry','blackberry','mango']
+print(thislist)
+rm = input("Ingrese una fruta + a la lista:\n")
+ps = int(input("Ingrese la posicion para la dicha fruta:\n"))
+thislist.insert(ps, rm)
+print(thislist)'''
+#--------------------------------------------------------------
